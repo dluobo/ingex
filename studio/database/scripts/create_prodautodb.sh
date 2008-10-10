806b55ef7de5f8ae6693989f8fe83b173fc10080
philipn <philipn>
2008-10-10 09:42:33 +0000

    only super user can create the plpgsql language for prodautodb

--- studio/database/scripts/create_prodautodb.sh
+++ studio/database/scripts/create_prodautodb.sh
@@ -1,4 +1,6 @@
 psql -v ON_ERROR_STOP=yes -U postgres -d template1 -c "CREATE DATABASE prodautodb"
 psql -v ON_ERROR_STOP=yes -U postgres -d template1 -c "GRANT CREATE ON DATABASE prodautodb TO bamzooki"
+psql -v ON_ERROR_STOP=yes -U postgres -d prodautodb -c "CREATE LANGUAGE plpgsql"
 psql -v ON_ERROR_STOP=yes -U bamzooki -d prodautodb -f ProdAutoDatabase.sql
+
 
