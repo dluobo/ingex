8b59de4537e6031c26c91ddcfc74d40d14c323f7
john_f <john_f>
2008-11-06 11:23:44 +0000

    Handle command-line option for install root dir (needed for rpmbuild).

--- studio/web/WebIngex/install.sh
+++ studio/web/WebIngex/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+# $Id: install.sh,v 1.3 2008/11/06 11:23:44 john_f Exp $
+#
 # Copyright (C) 2008  British Broadcasting Corporation
 # Author: Rowan de Pomerai <rdepom@users.sourceforge.net>
 #
@@ -18,43 +19,50 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-# very simple installation
+# One optional command line arg for the installation root directory
+# which defaults to /srv/www
+install_root=/srv/www
+if [ -n "$1" ] ; then
+	install_root=$1
+fi
 
 # Create the directories for CGI documents
-mkdir -p /srv/www/cgi-bin/ingex-modules
-mkdir -p /srv/www/cgi-bin/ingex-config
+mkdir -p $install_root/cgi-bin/ingex-modules || exit 1
+mkdir -p $install_root/cgi-bin/ingex-config || exit 1
 # Apply correct permissions to the directories
-chmod a+rx /srv/www/cgi-bin/ingex-modules
-chmod a+rx /srv/www/cgi-bin/ingex-config
+chmod a+rx $install_root/cgi-bin/ingex-modules || exit 1
+chmod a+rx $install_root/cgi-bin/ingex-config || exit 1
 # Copy the CGI files into these directories
-rsync -arC ingex-modules /srv/www/cgi-bin/
-rsync -arC ingex-config /srv/www/cgi-bin/
+rsync -arC ingex-modules $install_root/cgi-bin/ || exit 1
+rsync -arC ingex-config $install_root/cgi-bin/ || exit 1
 # Make all files read-only
-find /srv/www/cgi-bin/ingex-modules -type f -exec chmod 444 {} \;
-find /srv/www/cgi-bin/ingex-config -type f -exec chmod 444 {} \;
+find $install_root/cgi-bin/ingex-modules -type f -exec chmod 444 {} \; || exit 1
+find $install_root/cgi-bin/ingex-config -type f -exec chmod 444 {} \; || exit 1
 # Add exectue permissions to the Perl files
-find /srv/www/cgi-bin/ingex-modules -name "*.pl" -exec chmod 555 {} \;
-find /srv/www/cgi-bin/ingex-config -name "*.pl" -exec chmod 555 {} \;
+find $install_root/cgi-bin/ingex-modules -name "*.pl" -exec chmod 555 {} \;
+find $install_root/cgi-bin/ingex-config -name "*.pl" -exec chmod 555 {} \;
 # Make config files writable by the web server by changing their ownership (SuSE SPECIFIC!!!)
-find /srv/www/cgi-bin/ingex-modules -name "*.conf" -exec chown wwwrun:www {} \;
-find /srv/www/cgi-bin/ingex-modules -name "*.conf" -exec chmod 664 {} \;
-find /srv/www/cgi-bin/ingex-config -name "*.conf" -exec chown wwwrun:www {} \;
-find /srv/www/cgi-bin/ingex-config -name "*.conf" -exec chmod 664 {} \;
+find $install_root/cgi-bin/ingex-modules -name "*.conf" -exec chown wwwrun:www {} \;
+find $install_root/cgi-bin/ingex-modules -name "*.conf" -exec chmod 664 {} \;
+find $install_root/cgi-bin/ingex-config -name "*.conf" -exec chown wwwrun:www {} \;
+find $install_root/cgi-bin/ingex-config -name "*.conf" -exec chmod 664 {} \;
 
 # Create the directory for the HTDocs
-mkdir -p /srv/www/htdocs/ingex
+mkdir -p $install_root/htdocs/ingex || exit 1
 # Apply correct permissions to the directory
-chmod a+rx /srv/www/htdocs/ingex
+chmod a+rx $install_root/htdocs/ingex || exit 1
 # Copy the files into this directory
-rsync -arC htdocs/* /srv/www/htdocs/ingex
+rsync -arC htdocs/* $install_root/htdocs/ingex || exit 1
 # Put the favicon at the top level
-cp htdocs/favicon.ico /srv/www/htdocs
+cp htdocs/favicon.ico $install_root/htdocs || exit 1
 # Make all files read-only
-find /srv/www/htdocs/ingex -type f -exec chmod 444 {} \;
-chmod 444 /srv/www/htdocs/favicon.ico
+find $install_root/htdocs/ingex -type f -exec chmod 444 {} \; || exit 1
+chmod 444 $install_root/htdocs/favicon.ico || exit 1
 
 # tell the user what's happened
-echo "All files have been copied to the appropriate places in /srv/www/"
-echo "If you haven't already done so, you will need to edit the ingex.conf file to include your database details. e.g. sudo pico /srv/www/cgi-bin/ingex-config/ingex.conf"
-echo "You must also define your Ingex system's nodes (what machines you have at what IP addresses) on the Config page of WebIngex."
-
+echo "All files have been copied to the appropriate places in $install_root/"
+echo "If you haven't already done so, you will need to edit the ingex.conf file"
+echo "to include your database details. E.g."
+echo "  sudo vi $install_root/cgi-bin/ingex-config/ingex.conf"
+echo "You must also define your Ingex system's nodes (what machines you have at"
+echo "what IP addresses) on the Config page of WebIngex."
