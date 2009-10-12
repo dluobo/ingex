db3add1db71a16e2f5983626b5775c3c4240f27a
john_f <john_f>
2009-10-12 14:39:04 +0000

    Now using a struct for the timecode values etc.

--- common/nexus_control.c
+++ common/nexus_control.c
@@ -1,5 +1,5 @@
 /*
- * $Id: nexus_control.c,v 1.2 2009/03/25 13:46:37 john_f Exp $
+ * $Id: nexus_control.c,v 1.3 2009/10/12 14:39:04 john_f Exp $
  *
  * Module for creating and accessing nexus shared control memory
  *
@@ -21,7 +21,7 @@
  * 02110-1301, USA.
  */
 
-#define _XOPEN_SOURCE 600			// for posix_memalign
+#define _XOPEN_SOURCE 600           // for posix_memalign
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,7 +31,7 @@
 #include <math.h>
 
 #include <signal.h>
-#include <unistd.h>	
+#include <unistd.h> 
 #include <sys/time.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -45,150 +45,234 @@
 
 extern const char *nexus_capture_format_name(CaptureFormat fmt)
 {
-	const char *names[] = {
+    const char *names[] = {
     "FormatNone",
     "Format422UYVY",
     "Format422PlanarYUV",
     "Format422PlanarYUVShifted",
     "Format420PlanarYUV",
     "Format420PlanarYUVShifted"
-	};
+    };
 
-	return names[fmt];
+    return names[fmt];
 }
 
 extern const char *nexus_timecode_type_name(NexusTimecode tc_type)
 {
-	const char *names[] = {
-    "NexusTC_None",
-    "NexusTC_LTC",
-    "NexusTC_VITC"
-	};
+    const char * s;
+    switch (tc_type)
+    {
+    case NexusTC_LTC:
+        s = "LTC";
+        break;
+    case NexusTC_VITC:
+        s = "VITC";
+        break;
+    case NexusTC_DLTC:
+        s = "DLTC";
+        break;
+    case NexusTC_DVITC:
+        s = "DVITC";
+        break;
+    case NexusTC_SYSTEM:
+        s = "System";
+        break;
+    case NexusTC_DEFAULT:
+        s = "Default";
+        break;
+    default:
+        s = "Unknown";
+        break;
+    }
 
-	return names[tc_type];
+    return s;
 }
 
-extern int nexus_lastframe_num_aud_samp(const NexusControl *pctl, uint8_t *ring[], int channel)
+extern int nexus_lastframe(NexusControl *pctl, int channel)
 {
-	const NexusBufCtl *pc = &pctl->channel[channel];
-
-	return *(int*)(ring[channel] + pctl->elementsize * (pc->lastframe % pctl->ringlen)
-								+ pctl->num_aud_samp_offset);
+    int lastframe;
+    
+    PTHREAD_MUTEX_LOCK(&pctl->channel[channel].m_lastframe)
+    lastframe = pctl->channel[channel].lastframe;
+    PTHREAD_MUTEX_UNLOCK(&pctl->channel[channel].m_lastframe)
+    
+    return lastframe;
 }
 
-extern int nexus_lastframe_signal_ok(const NexusControl *pctl, uint8_t *ring[], int channel)
+extern NexusFrameData* nexus_frame_data(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
 {
-	const NexusBufCtl *pc = &pctl->channel[channel];
-
-	return *(int*)(ring[channel] + pctl->elementsize * (pc->lastframe % pctl->ringlen)
-								+ pctl->signal_ok_offset);
+    return (NexusFrameData *)(ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->frame_data_offset);
 }
 
-extern int nexus_lastframe_tc(const NexusControl *pctl, uint8_t *ring[], int channel, NexusTimecode tctype)
+extern int nexus_num_aud_samp(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
 {
-	const NexusBufCtl *pc = &pctl->channel[channel];
-
-	// If no timecode type specified use the master timecode type
-	// If that is not set, fallback to LTC
-	int tc_offset = pctl->ltc_offset;
-	if (tctype == NexusTC_None && pctl->master_tc_type != NexusTC_None) {
-		if (pctl->master_tc_type == NexusTC_VITC)
-			tc_offset = pctl->vitc_offset;
-	}
-
-	if (tctype == NexusTC_VITC)
-		tc_offset = pctl->vitc_offset;
-
-	return *(int*)(ring[channel] + pctl->elementsize * (pc->lastframe % pctl->ringlen) + tc_offset);
+    return nexus_frame_data(pctl, ring, channel, frame)->num_aud_samp;
 }
 
-extern const uint8_t *nexus_lastframe_audio12(const NexusControl *pctl, uint8_t *ring[], int channel)
+extern int nexus_signal_ok(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
 {
-	const NexusBufCtl *pc = &pctl->channel[channel];
-
-	return (ring[channel] + pctl->elementsize * (pc->lastframe % pctl->ringlen)
-								+ pctl->audio12_offset);
+    return nexus_frame_data(pctl, ring, channel, frame)->signal_ok;
 }
 
-extern const uint8_t *nexus_lastframe_audio34(const NexusControl *pctl, uint8_t *ring[], int channel)
+extern int nexus_frame_number(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
 {
-	const NexusBufCtl *pc = &pctl->channel[channel];
+    return nexus_frame_data(pctl, ring, channel, frame)->frame_number;
+}
 
-	return (ring[channel] + pctl->elementsize * (pc->lastframe % pctl->ringlen)
-								+ pctl->audio34_offset);
+extern int nexus_tc(const NexusControl *pctl, uint8_t *ring[], int channel, int frame, NexusTimecode tctype)
+{
+    NexusFrameData * nfd = nexus_frame_data(pctl, ring, channel, frame);
+
+    if (tctype == NexusTC_DEFAULT) {
+        tctype = pctl->default_tc_type;
+    }
+
+    int tc;
+    switch (tctype)
+    {
+    case NexusTC_VITC:
+        tc = nfd->vitc;
+        break;
+    case NexusTC_LTC:
+        tc = nfd->ltc;
+        break;
+    case NexusTC_DVITC:
+        tc = nfd->dvitc;
+        break;
+    case NexusTC_DLTC:
+        tc = nfd->dltc;
+        break;
+    case NexusTC_SYSTEM:
+        tc = nfd->systc;
+        break;
+    default:
+        tc = nfd->vitc;
+        break;
+    }
+
+    return tc;
+}
+
+extern const uint8_t *nexus_primary_video(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return ring[channel] + pctl->elementsize * (frame % pctl->ringlen);
+}
+
+extern const uint8_t *nexus_secondary_video(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->sec_video_offset;
+}
+
+extern const uint8_t *nexus_audio12(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return (ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->audio12_offset);
+}
+
+extern const uint8_t *nexus_audio34(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return (ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->audio34_offset);
+}
+
+extern const uint8_t *nexus_audio56(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return (ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->audio56_offset);
+}
+
+extern const uint8_t *nexus_audio78(const NexusControl *pctl, uint8_t *ring[], int channel, int frame)
+{
+    return (ring[channel] + pctl->elementsize * (frame % pctl->ringlen) + pctl->audio78_offset);
+}
+
+extern void nexus_set_source_name(NexusControl *pctl, int channel, const char *source_name)
+{
+    PTHREAD_MUTEX_LOCK(&pctl->m_source_name_update)
+    strncpy(pctl->channel[channel].source_name, source_name, sizeof(pctl->channel[channel].source_name));
+    pctl->channel[channel].source_name[sizeof(pctl->channel[channel].source_name) - 1] = '\0';   // guarantee null terminated string
+    pctl->source_name_update++;
+    PTHREAD_MUTEX_UNLOCK(&pctl->m_source_name_update)
+}
+
+extern void nexus_get_source_name(NexusControl *pctl, int channel, char *source_name, size_t source_name_size,
+                                  int *update_count)
+{
+    PTHREAD_MUTEX_LOCK(&pctl->m_source_name_update)
+    strncpy(source_name, pctl->channel[channel].source_name, source_name_size);
+    source_name[source_name_size - 1] = '\0';   // guarantee null terminated string
+    *update_count = pctl->source_name_update;
+    PTHREAD_MUTEX_UNLOCK(&pctl->m_source_name_update)
 }
 
 extern int nexus_connect_to_shared_mem(int timeout_microsec, int read_only, int verbose, NexusConnection *p)
 {
-	int			shm_id, control_id;
-	int			at_flags = 0;			// default 0 means read-write
+    int         shm_id, control_id;
+    int         at_flags = 0;           // default 0 means read-write
 
-	if (read_only)
-		at_flags = SHM_RDONLY;
+    if (read_only)
+        at_flags = SHM_RDONLY;
 
-	if (timeout_microsec > 0 && verbose) {
-		printf("Waiting for shared memory... ");
-		fflush(stdout);
-	}
+    if (timeout_microsec > 0 && verbose) {
+        printf("Waiting for shared memory... ");
+        fflush(stdout);
+    }
 
-	// If shared memory not found, sleep and try again
-	int retry_time = 20 * 1000;		// 20ms
-	int time_taken = 0;
-	while (1)
-	{
-		control_id = shmget(9, sizeof(NexusControl), 0444);
-		if (control_id != -1)
-			break;
+    // If shared memory not found, sleep and try again
+    int retry_time = 20 * 1000;     // 20ms
+    int time_taken = 0;
+    while (1)
+    {
+        control_id = shmget(9, sizeof(NexusControl), 0444);
+        if (control_id != -1)
+            break;
 
-		if (timeout_microsec == 0)
-			return 0;
+        if (timeout_microsec == 0)
+            return 0;
 
-		usleep(retry_time);
-		time_taken += retry_time;
-		if (time_taken >= timeout_microsec)
-			return 0;
-	}
+        usleep(retry_time);
+        time_taken += retry_time;
+        if (time_taken >= timeout_microsec)
+            return 0;
+    }
 
-	p->pctl = (NexusControl*)shmat(control_id, NULL, at_flags);
-	if (verbose)
-		printf("connected to pctl\n");
+    p->pctl = (NexusControl*)shmat(control_id, NULL, at_flags);
+    if (verbose)
+        printf("connected to pctl\n");
 
-	int i;
-	for (i = 0; i < p->pctl->channels; i++)
-	{
-		while (1)
-		{
-			shm_id = shmget(10 + i, p->pctl->elementsize, 0444);
-			if (shm_id != -1)
-				break;
-			usleep(20 * 1000);
-		}
-		p->ring[i] = (uint8_t*)shmat(shm_id, NULL, at_flags);
-		if (verbose)
-			printf("  attached to channel[%d]: '%s'\n", i, p->pctl->channel[i].source_name);
-	}
+    int i;
+    for (i = 0; i < p->pctl->channels; i++)
+    {
+        while (1)
+        {
+            shm_id = shmget(10 + i, p->pctl->elementsize, 0444);
+            if (shm_id != -1)
+                break;
+            usleep(20 * 1000);
+        }
+        p->ring[i] = (uint8_t*)shmat(shm_id, NULL, at_flags);
+        if (verbose)
+            printf("  attached to channel[%d]: '%s'\n", i, p->pctl->channel[i].source_name);
+    }
 
-	return 1;
+    return 1;
 }
 
 extern int nexus_disconnect_from_shared_mem(const NexusConnection *p)
 {
-	int i;
-	for (i = 0; i < p->pctl->channels; i++)
-	{
-		if (shmdt(p->ring[i]) == -1) {
-			fprintf(stderr, "p->ring[%d] (0x%p) ", i, p->ring[i]);
-			perror("shmdt");
-			return 0;
-		}
-	}
+    int i;
+    for (i = 0; i < p->pctl->channels; i++)
+    {
+        if (shmdt(p->ring[i]) == -1) {
+            fprintf(stderr, "p->ring[%d] (0x%p) ", i, p->ring[i]);
+            perror("shmdt");
+            return 0;
+        }
+    }
 
-	if (shmdt(p->pctl) == -1) {
-		fprintf(stderr, "p->pctl (0x%p) ", p->pctl);
-		perror("shmdt");
-		return 0;
-	}
-	return 1;
+    if (shmdt(p->pctl) == -1) {
+        fprintf(stderr, "p->pctl (0x%p) ", p->pctl);
+        perror("shmdt");
+        return 0;
+    }
+    return 1;
 }
 
 static int64_t tv_diff_microsecs(const struct timeval* a, const struct timeval* b)
@@ -200,39 +284,39 @@ static int64_t tv_diff_microsecs(const struct timeval* a, const struct timeval* 
 // Returns 1 if connection ok, 0 otherwise and sets return flags
 extern int nexus_connection_status(const NexusConnection *p, int *p_heartbeat_stopped, int *p_capture_dead)
 {
-	int heartbeat_stopped = 0;
-	int capture_dead = 0;
+    int heartbeat_stopped = 0;
+    int capture_dead = 0;
 
-	// p->pctl is NULL before first connection made
-	if (p->pctl) {
-		struct timeval now;
-		gettimeofday(&now, NULL);
-		int64_t diff = tv_diff_microsecs(&p->pctl->owner_heartbeat, &now);
-		// 100ms or lower can give false positives for lost heartbeat
-		if (diff > 160 * 1000) {
-			heartbeat_stopped = 1;
+    // p->pctl is NULL before first connection made
+    if (p->pctl) {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        int64_t diff = tv_diff_microsecs(&p->pctl->owner_heartbeat, &now);
+        // 100ms or lower can give false positives for lost heartbeat
+        if (diff > 160 * 1000) {
+            heartbeat_stopped = 1;
 
-			if (kill(p->pctl->owner_pid, 0) == -1) {
-				// If dvs_sdi is alive and running as root,
-				// kill() will fail with EPERM
-				if (errno != EPERM)
-					capture_dead = 1;
-			}
-		}
-	}
-	else {
-		heartbeat_stopped = 1;
-		capture_dead = 1;
-	}
+            if (kill(p->pctl->owner_pid, 0) == -1) {
+                // If dvs_sdi is alive and running as root,
+                // kill() will fail with EPERM
+                if (errno != EPERM)
+                    capture_dead = 1;
+            }
+        }
+    }
+    else {
+        heartbeat_stopped = 1;
+        capture_dead = 1;
+    }
 
-	if (heartbeat_stopped == 0 && capture_dead == 0)	// i.e. connection OK
-		return 1;
+    if (heartbeat_stopped == 0 && capture_dead == 0)    // i.e. connection OK
+        return 1;
 
-	if (p_heartbeat_stopped)
-		*p_heartbeat_stopped = heartbeat_stopped;
+    if (p_heartbeat_stopped)
+        *p_heartbeat_stopped = heartbeat_stopped;
 
-	if (p_capture_dead)
-		*p_capture_dead = capture_dead;
+    if (p_capture_dead)
+        *p_capture_dead = capture_dead;
 
-	return 0;
+    return 0;
 }
--- common/nexus_control.h
+++ common/nexus_control.h
@@ -1,5 +1,5 @@
 /*
- * $Id: nexus_control.h,v 1.4 2009/06/15 10:17:01 john_f Exp $
+ * $Id: nexus_control.h,v 1.5 2009/10/12 14:39:04 john_f Exp $
  *
  * Shared memory interface between SDI capture threads and reader threads.
  *
@@ -30,9 +30,11 @@
 #define PTHREAD_MUTEX_LOCK(x) if (pthread_mutex_lock( x ) != 0 ) fprintf(stderr, "pthread_mutex_lock failed\n");
 #define PTHREAD_MUTEX_UNLOCK(x) if (pthread_mutex_unlock( x ) != 0 ) fprintf(stderr, "pthread_mutex_unlock failed\n");
 
-#include <sys/time.h>		// gettimeofday() and struct timeval
+#include <sys/time.h>       // gettimeofday() and struct timeval
 #else
 #include <ace/OS_NS_sys_time.h>
+#define PTHREAD_MUTEX_LOCK(x)
+#define PTHREAD_MUTEX_UNLOCK(x)
 #endif
 
 #ifdef __cplusplus
@@ -49,123 +51,135 @@ typedef enum {
     Format420PlanarYUVShifted  // 4:2:0 buffer suitable for DV25 encoding (picture shift down 1 line)
 } CaptureFormat;
 
-// Currently supported types of capture timecodes. For example, the master timecode is used
-// to assign timecode to all non-master channels to ensure consistent timecodes.
+// Currently supported types of capture timecodes.
 typedef enum {
-	NexusTC_None,		// no timecode distribution
-	NexusTC_LTC,		// use LTC as master
-	NexusTC_VITC,		// use VITC as master
-	NexusTC_System		// system timecode using OS's time-of-day clock
+    NexusTC_LTC,        // LTC
+    NexusTC_VITC,       // VITC
+    NexusTC_DLTC,       // LTC from RP188/RP196 ANC data
+    NexusTC_DVITC,      // VITC from RP188/RP196 ANC data
+    NexusTC_SYSTEM,     // system timecode using OS's time-of-day clock
+    NexusTC_DEFAULT     // default type as set by capture option
 } NexusTimecode;
+
+typedef struct {
+    int vitc_bits; // timecode bits
+    int ltc_bits;
+    int dvitc_bits;
+    int dltc_bits;
+    int vitc;  // timecode as frame count
+    int ltc;
+    int dvitc;
+    int dltc;
+    int systc;
+    int tick;
+    int64_t timestamp; // microseconds since 1970
+    int signal_ok;
+    int num_aud_samp;
+    int frame_number;
+} NexusFrameData;
 
 // Each channel's ring buffer is described by the NexusBufCtl structure
 typedef struct {
 #ifndef _MSC_VER
-	pthread_mutex_t		m_lastframe;	// mutex for lastframe counter
-	// TODO: use a condition variable to signal when new frame arrives
+    pthread_mutex_t     m_lastframe;    // mutex for lastframe counter
+    // TODO: use a condition variable to signal when new frame arrives
 #endif
-	int		lastframe;			// last frame number stored and now available
-								// use lastframe % ringlen to get buffer index
-	int		hwdrop;				// frame-drops recorded by sv interface
-	double	hwtemperature;		// temperature of capture card hardware (degrees C)
-	int		num_audio_avail;	// number of audio channels configured and available for
-								// capture at the hardware level (not how many channels
-								// are carried by the current SDI signal, if any).
+    int     lastframe;          // last frame number stored and now available
+                                // use lastframe % ringlen to get buffer index
+    int     hwdrop;             // frame-drops recorded by sv interface
+    double  hwtemperature;      // temperature of capture card hardware (degrees C)
+    int     num_audio_avail;    // number of audio channels configured and available for
+                                // capture at the hardware level (not how many channels
+                                // are carried by the current SDI signal, if any).
 
-	// Following members are written to by Recorder
-	char			source_name[64];	// identifies the source for this channel
+    // Following members are written to by Recorder
+    char            source_name[64];    // identifies the source for this channel
 } NexusBufCtl;
 
-#define MAX_RECORDERS 2				// number of Recorders per machine
-#define MAX_ENCODES_PER_CHANNEL 2	// Recorder can make this number of encodes per channel
-#define MAX_CHANNELS 8				// number of separate video+audio inputs
+#define MAX_RECORDERS 2             // number of Recorders per machine
+#define MAX_ENCODES_PER_CHANNEL 2   // Recorder can make this number of encodes per channel
+#define MAX_CHANNELS 8              // number of separate video+audio inputs
 
 typedef struct {
-	int		enabled;			// set to 1 if channel and encoding is enabled
-	int		recording;			// set to 1 if channel is currently recording
-	int		record_error;		// set to 1 if an error occured during recording
-	int		frames_written;		// frames written during a recording
-	int		frames_dropped;		// frames dropped during a recording
-	int		frames_in_backlog;	// number of frames captured but not encoded yet
-	char	desc[64];			// string describing video format e.g. "DV25"
-	char	error[128];			// string representation of last error
+    int     enabled;            // set to 1 if channel and encoding is enabled
+    int     recording;          // set to 1 if channel is currently recording
+    int     record_error;       // set to 1 if an error occured during recording
+    int     frames_written;     // frames written during a recording
+    int     frames_dropped;     // frames dropped during a recording
+    int     frames_in_backlog;  // number of frames captured but not encoded yet
+    char    desc[64];           // string describing video format e.g. "DV25"
+    char    error[128];         // string representation of last error
 } NexusRecordEncodingInfo;
 
 typedef struct {
-	char					name[64];	// Recorder name e.g. "Ingex1"
-	int						pid;		// process id of Recorder to identify Recorders
-	NexusRecordEncodingInfo	channel[MAX_CHANNELS][MAX_ENCODES_PER_CHANNEL];
-	NexusRecordEncodingInfo	quad;
+    char                    name[64];   // Recorder name e.g. "Ingex1"
+    int                     pid;        // process id of Recorder to identify Recorders
+    NexusRecordEncodingInfo channel[MAX_CHANNELS][MAX_ENCODES_PER_CHANNEL];
+    NexusRecordEncodingInfo quad;
 } NexusRecordInfo;
 
 // Each element in a ring buffer is structured to facilitate the
 // DMA transfer from the capture card, and is structured as follows:
-//	video (4:2:2 primary)	- offset 0, size given by width*height*2
-//	audio channels 1,2		- offset given by audio12_offset
-//	audio channels 3,4		- offset given by audio34_offset
-//	audio channels 5,6 (optional)
-//	audio channels 7,8 (optional)
-//	(padding)
-//	signal status			- offset given by signal_ok_offset
-//	tick					- offset given by tick_offset
-//	timecodes				- offsets given by vitc_offset, ltc_offset
-//	video (4:2:0 secondary)	- offset given by sec_video_offset, size width*height*3/2
+//  video (4:2:2 primary)   - offset 0, size given by width*height*2
+//  audio channels 1,2      - offset given by audio12_offset
+//  audio channels 3,4      - offset given by audio34_offset
+//  audio channels 5,6 (optional)
+//  audio channels 7,8 (optional)
+//  (padding)
+//  signal status           - offset given by signal_ok_offset
+//  tick                    - offset given by tick_offset
+//  timecodes               - offsets given by vitc_offset, ltc_offset
+//  video (4:2:0 secondary) - offset given by sec_video_offset, size width*height*3/2
 
 // NexusControl is the top-level control struture describing how many channels
 // are in use and what their parameters are
 typedef struct {
-	NexusBufCtl		channel[MAX_CHANNELS];	// array of buffer control information
-											// for all 8 possible channels
+    NexusBufCtl     channel[MAX_CHANNELS];  // array of buffer control information
+                                            // for all 8 possible channels
 
-											// Stats updated by Recorders
-	NexusRecordInfo	record_info[MAX_RECORDERS];
+                                            // Stats updated by Recorders
+    NexusRecordInfo record_info[MAX_RECORDERS];
 
-	int				channels;			// number of channels and therefore ring buffers in use
-	int				ringlen;			// number of elements in ring buffer
-	int				elementsize;		// an element is video + audio + timecode
+    int             channels;           // number of channels and therefore ring buffers in use
+    int             ringlen;            // number of elements in ring buffer
+    int             elementsize;        // an element is video + audio + timecode
 
-	int				width;				// width of primary video
-	int				height;				// height of primary video
-	int				frame_rate_numer;	// frame rate numerator e.g. 25
-	int				frame_rate_denom;	// frame rate denominator e.g 1
-	CaptureFormat	pri_video_format;	// primary video format: usually UYVY, YUV422, ...
-	CaptureFormat	sec_video_format;	// secondary video format: usually NONE, YUV420, ...
-	int				sec_width;			// width of secondary video (if any)
-	int				sec_height;			// height of secondary video (if any)
-	NexusTimecode	master_tc_type;		// type of master timecode used (None, LTC, VITC, ...)
-	int				master_tc_channel;	// channel from which master timecode is sourced
-	int				owner_pid;			// process id of nominal owner of shared memory e.g. dvs_sdi
-	struct timeval	owner_heartbeat;	// heartbeat timestamp of owner of shared memory
+    int             width;              // width of primary video
+    int             height;             // height of primary video
+    int             frame_rate_numer;   // frame rate numerator e.g. 25
+    int             frame_rate_denom;   // frame rate denominator e.g 1
+    CaptureFormat   pri_video_format;   // primary video format: usually UYVY, YUV422, ...
+    CaptureFormat   sec_video_format;   // secondary video format: usually NONE, YUV420, ...
+    int             sec_width;          // width of secondary video (if any)
+    int             sec_height;         // height of secondary video (if any)
+    NexusTimecode   default_tc_type;    // type of timecode to be used by recorder etc.
+    int             master_tc_channel;  // channel from which master timecode is sourced; -1 for no master
+    int             owner_pid;          // process id of nominal owner of shared memory e.g. dvs_sdi
+    struct timeval  owner_heartbeat;    // heartbeat timestamp of owner of shared memory
 
-	// The following describe offsets within a ring buffer element for accessing data
-	int				audio12_offset;		// offset to start of audio ch 1,2 samples
-	int				audio34_offset;		// offset to start of audio ch 3,4 samples
-	int				audio56_offset;		// offset to start of audio ch 5,6 samples (if used)
-	int				audio78_offset;		// offset to start of audio ch 7,8 samples (if used)
-	int				audio_size;			// size in bytes of all audio data (4/8 chans)
-										// including internal padding for DMA transfer
-	int				frame_number_offset;// offset to frame number
-	int				num_aud_samp_offset;// offset to count of audio samples (varies for NTSC)
-	int				signal_ok_offset;	// offset to flag for good input status
-	int				tick_offset;		// offset to "frame" tick (dvs field tick / 2)
-	int				sys_time_offset;	// offset to start of system time timecode data (int)
-	int				ltc_offset;			// offset to start of LTC timecode data (int)
-	int				vitc_offset;		// offset to start of VITC timecode data (int)
-	int				sec_video_offset;	// offset to secondary video buffer
+    // The following describe offsets within a ring buffer element for accessing data
+    int             audio12_offset;     // offset to start of audio ch 1,2 samples
+    int             audio34_offset;     // offset to start of audio ch 3,4 samples
+    int             audio56_offset;     // offset to start of audio ch 5,6 samples (if used)
+    int             audio78_offset;     // offset to start of audio ch 7,8 samples (if used)
+    int             audio_size;         // size in bytes of all audio data (4/8 chans)
+                                        // including internal padding for DMA transfer
+    int             frame_data_offset;  // offset to struct with timecode and other data
+    int             sec_video_offset;   // offset to secondary video buffer
 
 
 #ifndef _MSC_VER
-	pthread_mutex_t	m_source_name_update;	// mutex for source_name_update
+    pthread_mutex_t m_source_name_update;   // mutex for source_name_update
 #endif
-	int				source_name_update;		// incremented each time the source_name is changed
+    int             source_name_update;     // incremented each time the source_name is changed
 
 } NexusControl;
 
 
 // Structure used by nexus_connect_to_shared_mem()
 typedef struct {
-	uint8_t			*ring[MAX_CHANNELS];
-	NexusControl	*pctl;
+    uint8_t         *ring[MAX_CHANNELS];
+    NexusControl    *pctl;
 } NexusConnection;
 
 extern int nexus_connect_to_shared_mem(int timeout_microsec, int read_only, int verbose, NexusConnection *p);
@@ -179,13 +193,28 @@ extern const char *nexus_capture_format_name(CaptureFormat fmt);
 // Return a char* string name for the NexusTimecode type
 extern const char *nexus_timecode_type_name(NexusTimecode tc_type);
 
-extern int nexus_lastframe_num_aud_samp(const NexusControl *pctl, uint8_t *ring[], int channel);
-extern int nexus_lastframe_signal_ok(const NexusControl *pctl, uint8_t *ring[], int channel);
+extern int nexus_lastframe(NexusControl *pctl, int channel);
 
-extern int nexus_lastframe_tc(const NexusControl *pctl, uint8_t *ring[], int channel, NexusTimecode tctype);
+extern NexusFrameData* nexus_frame_data(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
 
-extern const uint8_t *nexus_lastframe_audio12(const NexusControl *pctl, uint8_t *ring[], int channel);
-extern const uint8_t *nexus_lastframe_audio34(const NexusControl *pctl, uint8_t *ring[], int channel);
+extern int nexus_num_aud_samp(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern int nexus_signal_ok(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern int nexus_frame_number(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+
+extern int nexus_tc(const NexusControl *pctl, uint8_t *ring[], int channel, int frame, NexusTimecode tctype);
+
+extern const uint8_t *nexus_primary_video(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern const uint8_t *nexus_secondary_video(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+
+extern const uint8_t *nexus_audio12(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern const uint8_t *nexus_audio34(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern const uint8_t *nexus_audio56(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+extern const uint8_t *nexus_audio78(const NexusControl *pctl, uint8_t *ring[], int channel, int frame);
+
+extern void nexus_set_source_name(NexusControl *pctl, int channel, const char *source_name);
+
+extern void nexus_get_source_name(NexusControl *pctl, int channel, char *source_name, size_t source_name_size,
+                                  int *update_count);
 
 #ifdef __cplusplus
 }
