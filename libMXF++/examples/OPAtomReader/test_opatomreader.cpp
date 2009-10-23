b5a59be2e763b6182efd7a9dd699c37a31cfd02f
philipn <philipn>
2009-10-23 09:05:21 +0000

    Fix license text for libMXF/libMXF++ sources which should be LGPL

--- libMXF++/examples/OPAtomReader/test_opatomreader.cpp
+++ libMXF++/examples/OPAtomReader/test_opatomreader.cpp
@@ -1,27 +1,27 @@
 /*
- * $Id: test_opatomreader.cpp,v 1.1 2009/10/22 16:37:31 philipn Exp $
+ * $Id: test_opatomreader.cpp,v 1.2 2009/10/23 09:05:21 philipn Exp $
  *
- * Use to test the OP-Atom reader
+ * Test the OP-Atom reader
  *
  * Copyright (C) 2009  British Broadcasting Corporation.
  * All Rights Reserved.
  *
  * Author: Philip de Nier
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA.
+ * 02110-1301, USA
  */
 
 #define __STDC_FORMAT_MACROS    1
