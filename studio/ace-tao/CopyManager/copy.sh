5f71b6dc819c7e8861f8cdfaddaa9f6983128fd3
john_f <john_f>
2009-12-17 18:13:08 +0000

    No longer used.

--- studio/ace-tao/CopyManager/copy.sh
+++
@@ -1,1 +0,0 @@
-exec rsync -v -t --remove-sent-files /video/Avid\ MediaFiles/MXF/1/* /video/bigmo/
--- studio/ace-tao/CopyManager/run_client.sh
+++
@@ -1,2 +0,0 @@
- ./CopyManager "Studio A" -ORBDefaultInitRef corbaloc:iiop:pc118:8888 -ORBDottedDecimalAddresses 1
-
