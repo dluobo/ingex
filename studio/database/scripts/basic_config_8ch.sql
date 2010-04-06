df2686b6b74fdd664f5cc070af39ab651c5d9bea
john_f <john_f>
2010-04-06 17:24:40 +0000

    All recorder configs now have 8 audio tracks connected.

--- studio/database/scripts/basic_config_8ch.sql
+++ studio/database/scripts/basic_config_8ch.sql
@@ -156,67 +156,125 @@ SELECT setval('ric_id_seq', max(ric_identifier)) FROM recorderinputconfig;
 INSERT INTO recorderinputtrackconfig VALUES (100, 1, 1, 100, 100, 1);
 INSERT INTO recorderinputtrackconfig VALUES (101, 2, 1, 100, 100, 2);
 INSERT INTO recorderinputtrackconfig VALUES (102, 3, 2, 100, 100, 3);
-INSERT INTO recorderinputtrackconfig VALUES (103, 4, 0, 100, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (104, 5, 0, 100, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (103, 4, 3, 100, 100, 4);
+INSERT INTO recorderinputtrackconfig VALUES (104, 5, 4, 100, 100, 5);
+INSERT INTO recorderinputtrackconfig VALUES (105, 6, 5, 100, 100, 6);
+INSERT INTO recorderinputtrackconfig VALUES (106, 7, 6, 100, 100, 7);
+INSERT INTO recorderinputtrackconfig VALUES (107, 8, 7, 100, 100, 8);
+INSERT INTO recorderinputtrackconfig VALUES (108, 9, 8, 100, 100, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (110, 1, 1, 101, 101, 1);
 INSERT INTO recorderinputtrackconfig VALUES (111, 2, 1, 101, 101, 2);
 INSERT INTO recorderinputtrackconfig VALUES (112, 3, 2, 101, 101, 3);
-INSERT INTO recorderinputtrackconfig VALUES (113, 4, 0, 101, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (114, 5, 0, 101, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (113, 4, 3, 101, 101, 4);
+INSERT INTO recorderinputtrackconfig VALUES (114, 5, 4, 101, 101, 5);
+INSERT INTO recorderinputtrackconfig VALUES (115, 6, 5, 101, 101, 6);
+INSERT INTO recorderinputtrackconfig VALUES (116, 7, 6, 101, 101, 7);
+INSERT INTO recorderinputtrackconfig VALUES (117, 8, 7, 101, 101, 8);
+INSERT INTO recorderinputtrackconfig VALUES (118, 9, 8, 101, 101, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (120, 1, 1, 102, 102, 1);
 INSERT INTO recorderinputtrackconfig VALUES (121, 2, 1, 102, 102, 2);
 INSERT INTO recorderinputtrackconfig VALUES (122, 3, 2, 102, 102, 3);
-INSERT INTO recorderinputtrackconfig VALUES (123, 4, 0, 102, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (124, 5, 0, 102, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (123, 4, 3, 102, 102, 4);
+INSERT INTO recorderinputtrackconfig VALUES (124, 5, 4, 102, 102, 5);
+INSERT INTO recorderinputtrackconfig VALUES (125, 6, 5, 102, 102, 6);
+INSERT INTO recorderinputtrackconfig VALUES (126, 7, 6, 102, 102, 7);
+INSERT INTO recorderinputtrackconfig VALUES (127, 8, 7, 102, 102, 8);
+INSERT INTO recorderinputtrackconfig VALUES (128, 9, 8, 102, 102, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (130, 1, 1, 103, 103, 1);
 INSERT INTO recorderinputtrackconfig VALUES (131, 2, 1, 103, 103, 2);
 INSERT INTO recorderinputtrackconfig VALUES (132, 3, 2, 103, 103, 3);
-INSERT INTO recorderinputtrackconfig VALUES (133, 4, 0, 103, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (134, 5, 0, 103, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (133, 4, 3, 103, 103, 4);
+INSERT INTO recorderinputtrackconfig VALUES (134, 5, 4, 103, 103, 5);
+INSERT INTO recorderinputtrackconfig VALUES (135, 6, 5, 103, 103, 6);
+INSERT INTO recorderinputtrackconfig VALUES (136, 7, 6, 103, 103, 7);
+INSERT INTO recorderinputtrackconfig VALUES (137, 8, 7, 103, 103, 8);
+INSERT INTO recorderinputtrackconfig VALUES (138, 9, 8, 103, 103, 9);
 
 -- Ingex-Quicktime
 INSERT INTO recorderinputtrackconfig VALUES (200, 1, 1, 110, 100, 1);
 INSERT INTO recorderinputtrackconfig VALUES (201, 2, 1, 110, 100, 2);
 INSERT INTO recorderinputtrackconfig VALUES (202, 3, 2, 110, 100, 3);
-INSERT INTO recorderinputtrackconfig VALUES (203, 4, 0, 110, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (204, 5, 0, 110, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (203, 4, 3, 110, 100, 4);
+INSERT INTO recorderinputtrackconfig VALUES (204, 5, 4, 110, 100, 5);
+INSERT INTO recorderinputtrackconfig VALUES (205, 6, 5, 110, 100, 6);
+INSERT INTO recorderinputtrackconfig VALUES (206, 7, 6, 110, 100, 7);
+INSERT INTO recorderinputtrackconfig VALUES (207, 8, 7, 110, 100, 8);
+INSERT INTO recorderinputtrackconfig VALUES (208, 9, 8, 110, 100, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (210, 1, 1, 111, 101, 1);
 INSERT INTO recorderinputtrackconfig VALUES (211, 2, 1, 111, 101, 2);
 INSERT INTO recorderinputtrackconfig VALUES (212, 3, 2, 111, 101, 3);
-INSERT INTO recorderinputtrackconfig VALUES (213, 4, 0, 111, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (214, 5, 0, 111, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (213, 4, 3, 111, 101, 4);
+INSERT INTO recorderinputtrackconfig VALUES (214, 5, 4, 111, 101, 5);
+INSERT INTO recorderinputtrackconfig VALUES (215, 6, 5, 111, 101, 6);
+INSERT INTO recorderinputtrackconfig VALUES (216, 7, 6, 111, 101, 7);
+INSERT INTO recorderinputtrackconfig VALUES (217, 8, 7, 111, 101, 8);
+INSERT INTO recorderinputtrackconfig VALUES (218, 9, 8, 111, 101, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (220, 1, 1, 112, 102, 1);
 INSERT INTO recorderinputtrackconfig VALUES (221, 2, 1, 112, 102, 2);
 INSERT INTO recorderinputtrackconfig VALUES (222, 3, 2, 112, 102, 3);
-INSERT INTO recorderinputtrackconfig VALUES (223, 4, 0, 112, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (224, 5, 0, 112, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (223, 4, 3, 112, 102, 4);
+INSERT INTO recorderinputtrackconfig VALUES (224, 5, 4, 112, 102, 5);
+INSERT INTO recorderinputtrackconfig VALUES (225, 6, 5, 112, 102, 6);
+INSERT INTO recorderinputtrackconfig VALUES (226, 7, 6, 112, 102, 7);
+INSERT INTO recorderinputtrackconfig VALUES (227, 8, 7, 112, 102, 8);
+INSERT INTO recorderinputtrackconfig VALUES (228, 9, 8, 112, 102, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (230, 1, 1, 113, 103, 1);
 INSERT INTO recorderinputtrackconfig VALUES (231, 2, 1, 113, 103, 2);
 INSERT INTO recorderinputtrackconfig VALUES (232, 3, 2, 113, 103, 3);
-INSERT INTO recorderinputtrackconfig VALUES (233, 4, 0, 113, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (234, 5, 0, 113, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (233, 4, 3, 113, 103, 4);
+INSERT INTO recorderinputtrackconfig VALUES (234, 5, 4, 113, 103, 5);
+INSERT INTO recorderinputtrackconfig VALUES (235, 6, 5, 113, 103, 6);
+INSERT INTO recorderinputtrackconfig VALUES (236, 7, 6, 113, 103, 7);
+INSERT INTO recorderinputtrackconfig VALUES (237, 8, 7, 113, 103, 8);
+INSERT INTO recorderinputtrackconfig VALUES (238, 9, 8, 113, 103, 9);
+
 
 -- Ingex-HD-MXF
 INSERT INTO recorderinputtrackconfig VALUES (400, 1, 1, 200, 100, 1);
 INSERT INTO recorderinputtrackconfig VALUES (401, 2, 1, 200, 100, 2);
 INSERT INTO recorderinputtrackconfig VALUES (402, 3, 2, 200, 100, 3);
-INSERT INTO recorderinputtrackconfig VALUES (403, 4, 0, 200, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (404, 5, 0, 200, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (403, 4, 3, 200, 100, 4);
+INSERT INTO recorderinputtrackconfig VALUES (404, 5, 4, 200, 100, 5);
+INSERT INTO recorderinputtrackconfig VALUES (405, 6, 5, 200, 100, 6);
+INSERT INTO recorderinputtrackconfig VALUES (406, 7, 6, 200, 100, 7);
+INSERT INTO recorderinputtrackconfig VALUES (407, 8, 7, 200, 100, 8);
+INSERT INTO recorderinputtrackconfig VALUES (408, 9, 8, 200, 100, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (410, 1, 1, 201, 101, 1);
 INSERT INTO recorderinputtrackconfig VALUES (411, 2, 1, 201, 101, 2);
 INSERT INTO recorderinputtrackconfig VALUES (412, 3, 2, 201, 101, 3);
-INSERT INTO recorderinputtrackconfig VALUES (413, 4, 0, 201, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (414, 5, 0, 201, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (413, 4, 3, 201, 101, 4);
+INSERT INTO recorderinputtrackconfig VALUES (414, 5, 4, 201, 101, 5);
+INSERT INTO recorderinputtrackconfig VALUES (415, 6, 5, 201, 101, 6);
+INSERT INTO recorderinputtrackconfig VALUES (416, 7, 6, 201, 101, 7);
+INSERT INTO recorderinputtrackconfig VALUES (417, 8, 7, 201, 101, 8);
+INSERT INTO recorderinputtrackconfig VALUES (418, 9, 8, 201, 101, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (420, 1, 1, 202, 102, 1);
 INSERT INTO recorderinputtrackconfig VALUES (421, 2, 1, 202, 102, 2);
 INSERT INTO recorderinputtrackconfig VALUES (422, 3, 2, 202, 102, 3);
-INSERT INTO recorderinputtrackconfig VALUES (423, 4, 0, 202, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (424, 5, 0, 202, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (423, 4, 3, 202, 102, 4);
+INSERT INTO recorderinputtrackconfig VALUES (424, 5, 4, 202, 102, 5);
+INSERT INTO recorderinputtrackconfig VALUES (425, 6, 5, 202, 102, 6);
+INSERT INTO recorderinputtrackconfig VALUES (426, 7, 6, 202, 102, 7);
+INSERT INTO recorderinputtrackconfig VALUES (427, 8, 7, 202, 102, 8);
+INSERT INTO recorderinputtrackconfig VALUES (428, 9, 8, 202, 102, 9);
+
 INSERT INTO recorderinputtrackconfig VALUES (430, 1, 1, 203, 103, 1);
 INSERT INTO recorderinputtrackconfig VALUES (431, 2, 1, 203, 103, 2);
 INSERT INTO recorderinputtrackconfig VALUES (432, 3, 2, 203, 103, 3);
-INSERT INTO recorderinputtrackconfig VALUES (433, 4, 0, 203, NULL, NULL);
-INSERT INTO recorderinputtrackconfig VALUES (434, 5, 0, 203, NULL, NULL);
+INSERT INTO recorderinputtrackconfig VALUES (433, 4, 3, 203, 103, 4);
+INSERT INTO recorderinputtrackconfig VALUES (434, 5, 4, 203, 103, 5);
+INSERT INTO recorderinputtrackconfig VALUES (435, 6, 5, 203, 103, 6);
+INSERT INTO recorderinputtrackconfig VALUES (436, 7, 6, 203, 103, 7);
+INSERT INTO recorderinputtrackconfig VALUES (437, 8, 7, 203, 103, 8);
+INSERT INTO recorderinputtrackconfig VALUES (438, 9, 8, 203, 103, 9);
 
 -- Ingex-HD-Quicktime
 INSERT INTO recorderinputtrackconfig VALUES (500, 1, 1, 210, 100, 1);
