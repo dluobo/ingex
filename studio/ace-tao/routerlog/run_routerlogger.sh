0902a056fde36e089d8ea7c5db241cece6f8db82
john_f <john_f>
2010-11-23 16:34:27 +0000

    Updated to use case statement

--- studio/ace-tao/routerlog/run_routerlogger.sh
+++ studio/ace-tao/routerlog/run_routerlogger.sh
@@ -3,34 +3,64 @@
 # Choose config depending on location
 LOCATION=studio
 #LOCATION=stage
+#LOCATION=lot
+#LOCATION=dummy
+case $LOCATION in
 
-if [ "$LOCATION" = "studio" ]
-then
-# Studio
-# Example with just one area using the router
-# and communication via PC serial port.
+  ( studio )
+
 ./routerlogger --dbuser bamzooki --dbpass bamzooki \
-  -v \
-  -r /dev/ttyS0 -l \
-  -n Ingex-Router \
-  -m 9 \
-  -d "Cam1" -p 1 \
-  -d "Cam2" -p 2 \
-  -d "Cam3" -p 3 \
-  -d "Cam4" -p 4 \
-  -c "Studio multicam" \
-  -a corbaloc:iiop:172.29.154.23:8888/NameService \
+  -v -r 192.168.100.12:9001 -s -l \
+  -n Router-StudioA \
+  -m 20 \
+  -d "StudioA-Rx1" -p 1 \
+  -d "StudioA-Rx2" -p 2 \
+  -d "StudioA-Rx3" -p 3 \
+  -d "StudioA-Rx4" -p 4 \
+  -c "StudioA multi-camera clip" \
+  -a corbaloc:iiop:192.168.100.10:8888/NameService \
   -ORBDottedDecimalAddresses 1
+;;
 
-elif [ "$LOCATION" = "stage" ]
-then
+  ( stage )
 
-# Lot and Stage
+./routerlogger --dbuser bamzooki --dbpass bamzooki \
+  -v -r 192.168.100.22:9001 -s  \
+  -n Router-Stage \
+  -m 20 \
+  -d "Stage-Rx1" -p 1 \
+  -d "Stage-Rx2" -p 2 \
+  -d "Stage-Rx3" -p 3 \
+  -d "Stage-Rx4" -p 4 \
+  -c "Stage multi-camera clip" \
+  -a corbaloc:iiop:192.168.100.20:8888/NameService \
+  -ORBDottedDecimalAddresses 1
+;;
+
+  ( lot)
+
+./routerlogger --dbuser bamzooki --dbpass bamzooki \
+  -v -r 192.168.100.32:9001 -s  \
+  -n Router-Lot \
+  -m 20 \
+  -d "Lot-Rx1" -p 1 \
+  -d "Lot-Rx2" -p 2 \
+  -d "Lot-Rx3" -p 3 \
+  -d "Lot-Rx4" -p 4 \
+  -c "Lot multi-camera clip" \
+  -a corbaloc:iiop:192.168.100.30:8888/NameService \
+  -ORBDottedDecimalAddresses 1
+;;
+
+  ( example)
+
 # Example with two areas using same router
-# and communication via ethernet adapter.
+# router connection via serial port
+# and timecode from recorder shared memory
 ./routerlogger --dbuser bamzooki --dbpass bamzooki \
   -v \
-  -r 192.168.1.252:4098 -s \
+  -r /dev/ttyS0 \
+  -l \
   -n Router-Lot \
   -m 12 \
   -d "Lot-VT1" -p 5 \
@@ -48,11 +78,12 @@ then
   -c "Stage multi-camera clip" \
   -a corbaloc:iiop:192.168.1.130:8888/NameService \
   -ORBDottedDecimalAddresses 1
+;;
 
-else
+  ( * )
+echo "Unknown location"
+;;
 
-echo Unknown location.
-
-fi
+esac
 
 
