c8023140414dbf446bc879ebe78caba6a36a0e2b
john_f <john_f>
2011-04-18 09:37:34 +0000

    New file

---
+++ studio/ace-tao/Ingexgui/src/colours.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *   $Id: colours.h,v 1.1 2011/04/18 09:37:34 john_f Exp $             *
+ *                                                                         *
+ *   Copyright (C) 2011 British Broadcasting Corporation                   *
+ *   - all rights reserved.                                                *
+ *   Author: Matthew Marks                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _COLOURS_H_
+#define _COLOURS_H_
+
+#define BUTTON_WARNING_COLOUR wxColour(0xFF, 0x80, 0x00)
+#define RECORD_BUTTON_ENABLED_COLOUR wxColour(wxT("#B00000"))
+#define RECORD_BUTTON_RECORDING_COLOUR wxColour(wxT("RED"))
+#define VIDEO_TRACK_COLOUR wxColour(wxT("#00A000"))
+#define AUDIO_TRACK_COLOUR wxColour(wxT("#0000B0"))
+#define ROUTER_TRACK_COLOUR wxColour(wxT("#00A0A0"))
+#define NO_SIGNAL_COLOUR wxColour(wxT("#FF8000"))
+
+#endif
