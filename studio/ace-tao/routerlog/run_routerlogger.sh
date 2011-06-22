3dc9a255c61b1af550dc25c4a236560636a86c47
john_f <john_f>
2011-06-22 06:36:31 +0000

    Revised matrix output port numbers

--- studio/ace-tao/routerlog/run_routerlogger.sh
+++ studio/ace-tao/routerlog/run_routerlogger.sh
@@ -13,10 +13,10 @@ case $LOCATION in
   -v -r 192.168.100.12:9001 -s -l \
   -n Router-StudioA \
   -m 20 \
-  -d "StudioA-Rx1" -p 1 \
-  -d "StudioA-Rx2" -p 2 \
-  -d "StudioA-Rx3" -p 3 \
-  -d "StudioA-Rx4" -p 4 \
+  -d "StudioA-Sx1" -p 7 \
+  -d "StudioA-Sx2" -p 8 \
+  -d "StudioA-Sx3" -p 9 \
+  -d "StudioA-Sx4" -p 10 \
   -c "StudioA multi-camera clip" \
   -a corbaloc:iiop:192.168.100.10:8888/NameService \
   -ORBDottedDecimalAddresses 1
@@ -28,10 +28,10 @@ case $LOCATION in
   -v -r 192.168.100.22:9001 -s -l \
   -n Router-Stage \
   -m 20 \
-  -d "Stage-Rx1" -p 1 \
-  -d "Stage-Rx2" -p 2 \
-  -d "Stage-Rx3" -p 3 \
-  -d "Stage-Rx4" -p 4 \
+  -d "Stage-Sx1" -p 7 \
+  -d "Stage-Sx2" -p 8 \
+  -d "Stage-Sx3" -p 9 \
+  -d "Stage-Sx4" -p 10 \
   -c "Stage multi-camera clip" \
   -a corbaloc:iiop:192.168.100.20:8888/NameService \
   -ORBDottedDecimalAddresses 1
@@ -43,10 +43,10 @@ case $LOCATION in
   -v -r 192.168.100.32:9001 -s -l \
   -n Router-Lot \
   -m 20 \
-  -d "Lot-Rx1" -p 1 \
-  -d "Lot-Rx2" -p 2 \
-  -d "Lot-Rx3" -p 3 \
-  -d "Lot-Rx4" -p 4 \
+  -d "Lot-Sx1" -p 7 \
+  -d "Lot-Sx2" -p 8 \
+  -d "Lot-Sx3" -p 9 \
+  -d "Lot-Sx4" -p 10 \
   -c "Lot multi-camera clip" \
   -a corbaloc:iiop:192.168.100.30:8888/NameService \
   -ORBDottedDecimalAddresses 1
